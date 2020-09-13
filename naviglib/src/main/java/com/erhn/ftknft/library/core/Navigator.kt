6de9f559f4796a98c8826a977334cbf693d9e641
erhnftknft <fetakonfeta@gmail.com>
2020-09-13 15:01:09 +0300

    Adds KotlinDoc for Navigator.kt.

--- naviglib/src/main/java/com/erhn/ftknft/library/core/Navigator.kt
+++ naviglib/src/main/java/com/erhn/ftknft/library/core/Navigator.kt
@@ -1,8 +1,40 @@
 package com.erhn.ftknft.library.core
 
+/**
+ *
+ * An interface that describes the minimal commands for switching screens
+ *
+ */
 interface Navigator {
+
+    /**
+     * Replaces the current screen with a new one.
+     *
+     * @param[screen] screen that will replace the current.
+     *
+     */
     fun replace(screen: Screen)
+
+    /**
+     * Adds a new screen without replacement after the current one.
+     *
+     * @param[screen] Screen to be added.
+     *
+     */
     fun forward(screen: Screen)
+
+    /**
+     * Returns to the previous screen if any.
+     *
+     */
     fun back()
+
+    /**
+     * Returns the screen with tag @param[screenTag]
+     * If @param[screenTag] set to null then removes all screens.
+     *
+     * @param[screenTag] the screen to return to.
+     *
+     */
     fun backTo(screenTag: String?)
 }
